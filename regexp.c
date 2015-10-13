5113ad1166166bb3fe3f70f89520e96fcb51363c
Serge Voilokov <serge0x76@gmail.com>
2015-10-13 08:20:34 -0400

    Implement regex replace for buf.

--- regexp.c
+++ regexp.c
@@ -23,6 +23,8 @@ regex_compile(regex_t* rex, const char* pattern)
 	}
 }
 
+#ifdef REGEX_REPLACE
+
 void
 regex_replace(const char *rex_what, const char *replacement, char **text)
 {
@@ -51,4 +53,38 @@ regex_replace(const char *rex_what, const char *replacement, char **text)
 	regfree(&rex);
 
 	*text = s;
+}
+
+#endif
+
+void
+buf_replace(struct buf *buf, const char *rex_what, const char *replacement)
+{
+	regex_t rex;
+	regex_compile(&rex, rex_what);
+
+	regmatch_t match[1];
+
+	int rc = regexec(&rex, buf->s, 1, match, 0);
+	if (rc == REG_NOMATCH)
+		err(1, "no match. regex: %p", rex_what);
+
+	const char *orig = buf->s;
+	size_t len = buf->len - match[0].rm_eo + match[0].rm_so + 1;
+	if (replacement != NULL)
+		len += strlen(replacement);
+
+	char *s = calloc(1, len);
+	strncpy(s, orig, match[0].rm_so);  /* copy chunk before regexp */
+	if (replacement != NULL)
+		strcat(s, replacement);    /* copy replacement */
+	strcat(s, &orig[match[0].rm_eo]);  /* copy chunk after replacement */
+	s[len-1] = 0;
+
+	buf_clean(buf);
+
+	buf->s = s;
+	buf->len = len;
+	buf->cap = len;
+	regfree(&rex);
 }
--- regexp.h
+++ regexp.h
@@ -1,1 +1,3 @@
-void regex_replace(const char *rex_what, const char *replacement, char **text);
+#include "struct.h"
+
+void buf_replace(struct buf *buf, const char *rex_what, const char *replacement);
