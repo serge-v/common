bd0eae087cabc2a42ebdc567057fff20a16509fc
Serge Voilokov <serge0x76@gmail.com>
2015-11-20 15:45:55 +0000

    Added log warning and fatal functions.

--- log.c
+++ log.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <err.h>
+#include <string.h>
 
 static FILE *logger;
 
@@ -49,4 +52,73 @@ logi(const char *fmt, ...)
 	vprintf(fmt, args);
 	puts("");
 	va_end (args);
+}
+
+void
+logwarn(const char *fmt, ...)
+{
+	if (logger == NULL)
+		errx(1, "logger is not open");
+
+	time_t now = time(NULL);
+	char ts[50];
+	struct tm utctm;
+
+	gmtime_r(&now, &utctm);
+	strftime(ts, 50, "%F %T WARN  ", &utctm);
+
+	va_list args, cp;
+
+	va_start(args, fmt);
+	va_copy(cp, args);
+	fputs(ts, logger);
+	vfprintf(logger, fmt, args);
+	fputs("\n", logger);
+	va_end (args);
+	fflush(logger);
+
+	va_start(args, fmt);
+	va_copy(cp, args);
+	fprintf(stderr, "\x1b[32m%s", ts);
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, "\x1b[0m\n");
+	va_end (args);
+}
+
+void
+logfatal(const char *fmt, ...)
+{
+	if (logger == NULL)
+		errx(1, "logger is not open");
+
+	int err = errno;
+
+	time_t now = time(NULL);
+	char ts[50];
+	struct tm utctm;
+
+	gmtime_r(&now, &utctm);
+	strftime(ts, 50, "%F %T FATAL ", &utctm);
+
+	va_list args, cp;
+
+	va_start(args, fmt);
+	va_copy(cp, args);
+	fputs(ts, logger);
+	vfprintf(logger, fmt, args);
+	if (err > 0)
+		fprintf(logger, ": %d: %s", err, strerror(err));
+	fputs("\n", logger);
+	va_end (args);
+	fflush(logger);
+
+	va_start(args, fmt);
+	va_copy(cp, args);
+	fprintf(stderr, "\x1b[31m%s", ts);
+	vfprintf(stderr, fmt, args);
+	if (err > 0)
+		fprintf(stderr, ": %d: %s", err, strerror(err));
+	fprintf(stderr, "\x1b[0m\n");
+	va_end (args);
+	exit(1);
 }
--- log.h
+++ log.h
@@ -1,3 +1,5 @@
 void log_open(const char *fname);
 void log_close();
 void logi(const char *fmt, ...);
+void logwarn(const char *fmt, ...);
+void logfatal(const char *fmt, ...);
